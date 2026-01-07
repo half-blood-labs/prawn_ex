76c16d736b0ce7b0c873f488a3179cd2cd83b76e
Junaid Farooq <fjunaid252@gmail.com>
2026-01-07 13:22:10 +0500

    chore: add MIT LICENSE and package.licenses

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule PrawnEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{}
     ]
   end
 
